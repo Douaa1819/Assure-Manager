94e403b7da84c6cf2e01d2fbca8191566aa43eed
Douaa <douaachemnane@gmail.com>
2024-10-24 03:07:09 +0100

    AM-8: Implement registration and login functionalities with DTOs and MapStruct, handle exceptions and configure BCrypt for password hashing

--- src/main/java/org/assuremanager/Main.java
+++ src/main/java/org/assuremanager/Main.java
@@ -8,25 +8,26 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(JpaConfig.class);
+//        ApplicationContext context = new AnnotationConfigApplicationContext(JpaConfig.class);
+//
+//        UserService userService = context.getBean(UserService.class);
+//
+//
+//        // Example usage
+//        User user = new User();
+//        user.setName("John Doe");
+//        user.setEmail("john.doe@example.com");
+//        user.setPassword("password");
+//        user.setAddress("address");
+//        user.setPhoneNumber("121");
+//
+//        try {
+//            userService.save(user);
+//            System.out.println("User saved successfully: " + user.getName());
+//        } catch (Exception e) {
+//            System.out.println("Error saving user: " + e.getMessage());
+//        }
+//    }
 
-        UserService userService = context.getBean(UserService.class);
-
-
-        // Example usage
-        User user = new User();
-        user.setName("John Doe");
-        user.setEmail("john.doe@example.com");
-        user.setPassword("password");
-        user.setAddress("address");
-        user.setPhoneNumber("121");
-
-        try {
-            userService.save(user);
-            System.out.println("User saved successfully: " + user.getName());
-        } catch (Exception e) {
-            System.out.println("Error saving user: " + e.getMessage());
-        }
     }
-
 }
